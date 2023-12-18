0dea75519f1b76330b895d29fd61debb0387b1ba
maturi karthik <maturikarthik@maturis-MacBook-Pro.local>
2023-12-18 18:29:42 +0530

    adding Project Link

--- model/menu.js
+++ model/menu.js
@@ -13,6 +13,10 @@ const menu = [
     label: "react jS",
     path: "/reactjs",
   },
+  {
+    label: "react projects",
+    path: "https://github.com/eugoteams/react_session_projects/tree/main/src",
+  },
 ];
 
 export default menu;
